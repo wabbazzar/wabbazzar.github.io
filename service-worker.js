9567e4b43b1a4fa24ccc795ced81841be218110b
wabbazzar <wesleybeckner@gmail.com>
2026-08-25 10:48:53 -0500

    Add podcast RSS feed with episode 01

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
 //
 // Bump VERSION to invalidate old caches on next activation.
 
-const VERSION = 'v27';
+const VERSION = 'v28';
 const CACHE_STATIC = `wz-static-${VERSION}`;
 const CACHE_RUNTIME = `wz-runtime-${VERSION}`;
 
